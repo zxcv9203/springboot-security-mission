64a1f8cf6944281ebd313b9536a07c77e7e2eae7
yongckim <zxcv9203@naver.com>
2022-05-16 16:44:54 +0900

    mission1 : login account add

--- src/main/java/com/programmers/devcourse/config/WebSecurityConfig.java
+++ src/main/java/com/programmers/devcourse/config/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package com.programmers.devcourse.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -26,5 +30,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/me")
                 .permitAll()
                 .and();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("{noop}user123").roles("USER").and()
+                .withUser("admin").password("{noop}admin123").roles("ADMIN");
     }
 }
