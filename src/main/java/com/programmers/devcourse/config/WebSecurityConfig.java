e3b6f16e327a5e9fd3ad26da36d03b4aa402365c
yongckim <zxcv9203@naver.com>
2022-05-16 15:46:51 +0900

    day01 : spirngboot-security

---
+++ src/main/java/com/programmers/devcourse/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.programmers.devcourse.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/assets/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+                .anyRequest().permitAll()
+                .and()
+            .formLogin()
+                .defaultSuccessUrl("/me")
+                .permitAll()
+                .and();
+    }
+}
