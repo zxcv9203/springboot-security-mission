5cded92a3fa68f8e0c8b405258286b446ad73ac6
yongckim <zxcv9203@naver.com>
2022-05-18 16:42:14 +0900

    day03 done

--- src/main/java/com/programmers/devcourse/config/WebSecurityConfig.java
+++ src/main/java/com/programmers/devcourse/config/WebSecurityConfig.java
@@ -1,18 +1,40 @@
 package com.programmers.devcourse.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final Logger log = LoggerFactory.getLogger(WebSecurityConfig.class);
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, e) -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Object principal = authentication != null ? authentication.getPrincipal() : null;
+            log.warn("{} is denied", principal, e);
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("text/plain");
+            response.getWriter().write("## ACCESS DENIED ##");
+            response.getWriter().flush();
+            response.getWriter().close();
+        };
+    }
 
     @Override
     public void configure(WebSecurity web) {
@@ -24,17 +46,43 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/admin").access("hasRole('ADMIN') and isFullyAuthenticated()")
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
                 .defaultSuccessUrl("/me")
                 .permitAll()
                 .and()
+                /**
+                 * rememberMe 설정
+                 */
                 .rememberMe()
                 .tokenValiditySeconds(300)
                 .and()
+                /**
+                 * 로그아웃 설정
+                 */
                 .logout()
-                .logoutSuccessUrl("/");
+                .logoutSuccessUrl("/")
+                .and()
+                /**
+                 * HTTP -> HTTPS 요청으로 리다이렉트
+                 */
+                .requiresChannel()
+                .anyRequest().requiresSecure()
+                .and()
+                /**
+                 * anonymous 설정
+                 */
+                .anonymous()
+                .principal("thisIsAnonymousUser")
+                .authorities("ROLE_ANONYMOUS", "ROLE_UNKNOWN")
+                .and()
+                /**
+                 * 커스텀 에러 핸들링
+                 */
+                .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler());
     }
 
     @Override
